917d15a4024e18783a1024e692926893a5becbd0
david-storm <venia7vedmak@gmail.com>
2019-10-28 15:58:47 +0200

    select + slaider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-const rexExpEmail = /[\w\.-_]+@[\w]+\.[A-Za-z]{2,3}$/; 
+const rexExpEmail = /[\w\.-_]+@[\w]+\.[A-Za-z]{2,3}$/;
 const rexExpPassword = /.{8,}$/;
 const rexExpNameAndHobbi = /[\w,\.-_]{3,}$/;
-const rexExpHouse = /[1-9]$/;
 const inputEmail = document.getElementById("email");
 const inputPassword = document.getElementById("password");
 const formFirst = document.getElementById("formFirst");
@@ -10,43 +9,44 @@ const inputName = document.getElementById("name");
 const inputHouse = document.getElementById("house");
 const inputHobbi = document.getElementById("hobbi");
 
+
 //function for field validation
-function validField(selector, regExp, event="keyup"){
-    selector.addEventListener("blur", ()  => {
+function validField(selector, regExp, event = "keyup") {
+    selector.addEventListener("blur", () => {
         const value = selector.value;
-        if(!regExp.test(value)){
-            selector.nextElementSibling.classList.add("error");  
-        } 
-        selector.addEventListener(event, ()  => {
+        if (!regExp.test(value)) {
+            selector.nextElementSibling.classList.add("error");
+        }
+        selector.addEventListener(event, () => {
             const value = selector.value;
-            if(!regExp.test(value)){
-                selector.nextElementSibling.classList.add("error");         
-            } else{
-                selector.nextElementSibling.classList.remove("error");   
+            if (!regExp.test(value)) {
+                selector.nextElementSibling.classList.add("error");
+            } else {
+                selector.nextElementSibling.classList.remove("error");
             }
         });
-        
+
     });
-    if(regExp.test(selector.value)){
+    if (regExp.test(selector.value)) {
         return true;
     }
 }
 
 //call function to assign handler
 validField(inputEmail, rexExpEmail);
-validField(inputPassword,rexExpPassword);
+validField(inputPassword, rexExpPassword);
 let message = false;
 
-formFirst.addEventListener("submit" ,(event) =>{
-   
+formFirst.addEventListener("submit", (event) => {
+
     //call the function to get the validation result
-    if(validField(inputEmail, rexExpEmail) === true && 
-        validField(inputPassword,rexExpPassword) === true){
+    if (validField(inputEmail, rexExpEmail) === true &&
+        validField(inputPassword, rexExpPassword) === true) {
         formFirst.classList.add("formHide");
         formSecond.classList.remove("formHide");
         message = false;
     } else {
-        if(!message){
+        if (!message) {
             message = document.createElement('label');
             message.innerHTML = "Please fill in all fields correctly";
             message.classList.add("errorLogin");
@@ -58,18 +58,17 @@ formFirst.addEventListener("submit" ,(event) =>{
 
 //call function to assign handler
 validField(inputName, rexExpNameAndHobbi);
-validField(inputHouse, rexExpHouse, "change");
 validField(inputHobbi, rexExpNameAndHobbi);
 
-formSecond.addEventListener("submit" ,(event) =>{
-   event.preventDefault();
+formSecond.addEventListener("submit", (event) => {
+    event.preventDefault();
     //call the function to get the validation result
-    if(validField(inputName, rexExpNameAndHobbi) && 
-    validField(inputHouse, rexExpHouse, "change") &&
-    validField(inputHobbi, rexExpNameAndHobbi)){
+    if (validField(inputName, rexExpNameAndHobbi) &&
+        validField(inputHouse, rexExpHouse, "change") &&
+        validField(inputHobbi, rexExpNameAndHobbi)) {
         alert("The form is filled perfectly");
-    }else {
-        if(!message){
+    } else {
+        if (!message) {
             message = document.createElement('label');
             message.innerHTML = "Please fill in all fields correctly";
             message.classList.add("errorLogin");
@@ -79,19 +78,38 @@ formSecond.addEventListener("submit" ,(event) =>{
 });
 
 $('.slaider').slick({
-        dots: false,
-        infinite: true,
-        arrows: false,
-        autoplay: false,
-        autoplaySpeed: 3000,
-        waitForAnimate: false,
+    draggable: false,
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    waitForAnimate: false,
 });
 
+$('#house').niceSelect();
 
+const allHouses = {
+    "Lannister of Casterly Rock": 0,
+    "Greyjoy of Pyke": 1,
+    "Arryn of the Eyrie": 2,
+    "Baratheon of Storm's End": 3,
+    "Martell of Sunspear": 4,
+    "Stark of Winterfell": 5,
+    "Targaryen of King's Landing": 6,
+    "Tully of Riverrun": 7,
+    "Tyrell of Highgarden": 8
+};
 
-// $('#house').select2();
-$('#house').select2({
-    width: '100%',
-    // theme: 'bootstrap4',
-    minimumResultsForSearch: -1,
+$('#house').change(() => {
+        let house = $(".current").html();
+    if (house === "Select House") {
+        $('.slaider').slick('slickPlay');
+        $('#house').next().next().addClass("error");
+    } else {
+        $('#house').next().next().removeClass("error");
+        let index = allHouses[house];
+        $('.slaider').slick('slickPause');
+        $('.slaider').slick('slickGoTo', index);
+    }
 });

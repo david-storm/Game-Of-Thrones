c52d7e0cd0a26b0c77e645fcdbcdae7f9f5f2135
david-storm <venia7vedmak@gmail.com>
2019-11-17 23:56:05 +0200

    Finished PS4

--- index.php
+++ index.php
@@ -1,22 +1,66 @@
 <?php
-
 session_start();
 
+
+//if there is a submit, check what form it came from
+if (isset($_POST) && isset($_POST['submit'])) {
+    
+    if ($_POST['submit'] == 'Sign Up') {
+        $result = loginOrRegUser();
+        if (isset($result['res']) && $result['res']) {
+            nextForm();
+        } else {
+            $GLOBALS['message'] = $result['message'];
+        }
+    }
+    if ($_POST['submit'] == 'Save') {
+        $data = validUserData();
+        writeToSession($data);
+        if ($data['valid']) {
+            unset($data['valid']);
+            saveUserData($data);
+            unset($_SESSION['current_user']);
+            unset($_SESSION['form']);
+        }
+    }
+}
+
+//form validation and authentication
+function loginOrRegUser() {
+    $result = array();
+    
+    $email = isset($_POST['email']) ? strval($_POST['email']) : '';
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $result['message']['email'] = 'E-mail is not valid';
+    }
+    
+    $password = isset($_POST['password']) ? strval($_POST['password']) : '';
+    if (strlen($password) < 8) {
+        $result['message']['password'] = 'Password is short';
+    }
+    
+    if (empty($result)) {
+        $_SESSION['current_user']['email'] = $email;
+        $result = auth_user($email, $password);
+    }
+    return $result;
+}
+
+//Check if there is an email in the database and verify the password, or register a new user
 function auth_user($email, $password) {
 
     $fileName = $email . '.json';
     $fullName = 'db_users/' . $fileName;
+    
     if (in_array($fileName, scandir('db_users'))) {
         $handle = fopen($fullName, 'r');
         $passInDB = json_decode(fread($handle, filesize($fullName)), TRUE)['password'];
         if ($passInDB == $password) {
             $result = array('res' => 1);
-        }
-        else {
+        } else {
             $result = array('res' => 0, 'message' => array('password' => 'Wrong password'));
         }
-    }
-    else {
+    } else {
 
         $handle = fopen($fullName, 'x');
         $data = array('password' => $password);
@@ -27,20 +71,8 @@ function auth_user($email, $password) {
     return $result;
 }
 
-function loginOrRegUser() {
-    $email = isset($_POST['email']) ? strval($_POST['email']) : '';
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return array('res' => false, 'message' => array('email' => 'E-mail is not valid'));
-    }
-    $password = isset($_POST['password']) ? strval($_POST['password']) : '';
-    if (strlen($password) < 8) {
-        return array('res' => false, 'message' => array('password' => 'Password is short'));
-    }
-    $_SESSION['current_user'] = array('email' => $email);
-    $result = auth_user($email, $password);
-    return $result;
-}
 
+//form 2, data validation
 function validUserData() {
     $message = array();
     $validData = array();
@@ -48,45 +80,38 @@ function validUserData() {
         $validName = preg_match('/^[A-Z][a-z]{1,15}$/', $name);
         if ($validName) {
             $validData['name'] = $name;
-        }
-        else {
+        } else {
             $message['name'] = 'Your name must begin with a capital letter, from 2 to 16 letter long, must not contain special characters and spaces.';
         }
-    }
-    else {
+    } else {
         $message['name'] = 'You did not fill in the field name.';
     }
 
     if (isset($_POST['house']) && $house = intval($_POST['house'])) {
         if ($house > 0 && $house < 10) {
             $validData['house'] = $house;
-        }
-        else {
+        } else {
             $message['house'] = 'You must choose one of the listed houses .';
         }
-    }
-    else {
+    } else {
         $message['house'] = 'You did not fill in the field house.';
     }
 
     if (isset($_POST['hobbi']) && $hobbi = $_POST['hobbi']) {
         $validHobbi = preg_match('/^.{3,}?\h.{3,}/', $hobbi);
-        if (!$validHobbi) {
+        if ($validHobbi) {
             $validData['hobbi'] = $hobbi;
-        }
-        else {
+        } else {
             $message['hobbi'] = 'You must write at least 2 hobbies, each hobia must be longer than 2 characters.';
         }
-    }
-    else {
+    } else {
         $message['hobbi'] = 'You did not fill in the field hobbi.';
     }
 
     if (empty($message)) {
         $validData['valid'] = TRUE;
         return $validData;
-    }
-    else {
+    } else {
         $validData['valid'] = FALSE;
         $GLOBALS['message'] = $message;
         return $validData;
@@ -100,34 +125,18 @@ function writeToSession($data) {
 }
 
 function saveUserData($data) {
-    $fullName = 'db_users/' . $_SESSION['current_user']['email'];
-     
-    $handle = fopen($fullName, 'x');    
+    $fullName = 'db_users/' . $_SESSION['current_user']['email'] . '.json';
+    echo $fullName;
+    $handle = fopen($fullName, 'r+');
+    $data['password'] = json_decode(fread($handle, filesize($fullName)), TRUE)['password'];
+    ftruncate($handle,0);
+    rewind($handle);
     fwrite($handle, json_encode($data));
-       
-    
-    fclose($handle); 
+
+
+    fclose($handle);
 }
 
-if (isset($_POST) && isset($_POST['submit'])) {
-    if ($_POST['submit'] == 'Sign Up') {
-        $result = loginOrRegUser();
-        if ($result['res']) {
-            nextForm();
-        } else {
-            $GLOBALS['message'] = $result['message'];
-        }
-    }
-    if ($_POST['submit'] == 'Save') {
-        $data = validUserData();
-        writeToSession($data);
-        if ($data['valid']) {
-            unset($data['valid']);
-            saveUserData($data);
-            unset($_SESSION['form']);
-        } 
-    }
-}
 
 $house = array(
     "Lannister of Casterly Rock",
@@ -161,17 +170,17 @@ function viewsForm() {
     $messageEmail = empty($GLOBALS['message']['email']) ? '' : $GLOBALS['message']['email'];
     $messagePassword = empty($GLOBALS['message']['password']) ? '' : $GLOBALS['message']['password'];
     $messageName = empty($GLOBALS['message']['name']) ? '' : $GLOBALS['message']['name'];
-    $messageHouse = empty($GLOBALS['message']['house']) ? '' : $GLOBALS['message']['house'];  
+    $messageHouse = empty($GLOBALS['message']['house']) ? '' : $GLOBALS['message']['house'];
     $messageHobbi = empty($GLOBALS['message']['hobbi']) ? '' : $GLOBALS['message']['hobbi'];
 
     $formFirst = '<form  id="formFirst" action="index.php" method="POST">
             <label for="email">Enter your email</label>
             <input type="email" name="email" id="email" placeholder="arya@westeros.com" value="' . $email . '">
-            ' . $messageEmail . '
+            <div class="wrongField">' . $messageEmail . '</div>
             <hr color="#d3bb89">
             <label for="password">Choose secure password<br><span>Must be least 8 characters</span></label>
             <input type="password" name="password" placeholder="password" minlength="8" id="password">
-             ' . $messagePassword . '
+            <div class="wrongField">' . $messagePassword . '</div>
             <hr color="#d3bb89">
             <label >
                 <input type="checkbox" class="checkbox" name="remember" id="checkbox"><span class="mycheckbox"></span>
@@ -185,7 +194,7 @@ function viewsForm() {
                 Tell us more about yourself.</p>
             <label for="name">Who are you?<br><span>Alpha-numeric username</span></label>
             <input type="text" name="name" id="name" placeholder="arya" value="' . $name . '">
-                 ' . $messageName . '
+                  <div class="wrongField">' . $messageName . '</div>
             <hr color="#d3bb89">
             <label for="house">Your Great House</label>
             <select id="house" name="house" value="' . $house . '">
@@ -200,11 +209,11 @@ function viewsForm() {
 		<option value="8">Tyrell of Highgarden</option>
 		<option value="9">Martell of Sunspear</option>
             </select>
-             ' . $messageHouse . '
+             <div class="wrongField">' . $messageHouse . '</div>
             <hr color="#d3bb89">
             <label for="hobbi">Your preferences, hobbies, wishes, etc.</label>
-            <textarea id="hobbi" name="hobbi" rows="2" placeholder="I have a long TOKILL list..." value="' . $hobbi . '"></textarea>
-            ' . $messageHobbi . '
+            <textarea id="hobbi" name="hobbi" rows="2" placeholder="I have a long TOKILL list...">' . $hobbi . '</textarea>
+             <div class="wrongField">' . $messageHobbi . '</div>
             <hr color="#d3bb89">
             <input type="submit" name="submit" value="Save" id="save" class="button">
 	</form>';
